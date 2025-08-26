1d97bba79cbeaa0aa637dd55c0254f0f0a1cc900
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-26 05:55:38 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+bootstrapApplication(App, appConfig,
+  {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]  
+  })
+  
   .catch((err) => console.error(err));
