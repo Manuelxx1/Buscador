c30929b945f1c06003ea821a27b7ad0333eabd94
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-26 03:44:45 -0300

    Update busqueda.ts

--- src/app/busqueda/busqueda.ts
+++ src/app/busqueda/busqueda.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-busqueda',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule] ,
   templateUrl: './busqueda.html',
   styleUrl: './busqueda.css'
 })
 export class Busqueda {
+  
+busquedaControl = new FormControl('');
+  resultado: string | null = null;
 
+  buscar() {
+    const valor = this.busquedaControl.value?.trim();
+    if (valor) {
+      // Simulamos una búsqueda
+      this.resultado = `Buscaste: "${valor}"`;
+    } else {
+      this.resultado = 'No ingresaste ningún término.';
+    }
+  }
 }
