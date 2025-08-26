3bf48f998dc7e6a31978a0f5b61a8fb19518dabe
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-26 05:36:39 -0300

    Update busquedaservice.ts

--- src/app/busquedaservice.ts
+++ src/app/busquedaservice.ts
@@ -1,11 +1,25 @@
 
 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class Busquedaservice {
+  private apiUrl = 'https://8080-cs-a039ce25-3610-425a-9d0a-fbf343f80023.cs-us-east1-pkhd.cloudshell.dev/html-link' ; // Ajustá según tu backend
+
+  constructor(private http: HttpClient) {}
+
+  buscar(frase: string): Observable<string> {
+    return this.http.get(this.apiUrl, {
+      params: { frase },
+      responseType: 'text' // Porque el endpoint devuelve HTML como texto
+    });
+  }
   
 }
 
