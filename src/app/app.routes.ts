0217231640f26740a02112185e3b3e3c4f34d6de
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-26 03:50:45 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Busqueda} from './busqueda';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'buscar', component: Busqueda }
+];
