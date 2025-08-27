a5946acaf424dab5750484d10603f8d471b6e1f9
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-27 04:22:08 -0300

    Update busquedaservice.ts

--- src/app/busquedaservice.ts
+++ src/app/busquedaservice.ts
@@ -10,7 +10,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Busquedaservice {
-  private apiUrl = 'https://8080-cs-a039ce25-3610-425a-9d0a-fbf343f80023.cs-us-east1-pkhd.cloudshell.dev/html-link' ; // Ajustá según tu backend
+  //private apiUrl = 'https://8080-cs-a039ce25-3610-425a-9d0a-fbf343f80023.cs-us-east1-pkhd.cloudshell.dev/html-link' ; 
+private apiUrl = 'http://localhost:8080/html-link';
 
   constructor(private http: HttpClient) {}
 
