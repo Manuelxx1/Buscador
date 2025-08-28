fe36c2cd0061897ac0229da0852df0ae01b33540
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-28 00:59:24 -0300

    Update busquedaservice.ts

--- src/app/busquedaservice.ts
+++ src/app/busquedaservice.ts
@@ -13,7 +13,7 @@ export class Busquedaservice {
   //private apiUrl = 'https://8080-cs-a039ce25-3610-425a-9d0a-fbf343f80023.cs-us-east1-pkhd.cloudshell.dev/html-link' ; 
 private apiUrl = 'http://localhost:8080/html-link';
 
-  constructor(private http: HttpClient
+  constructor(private http: HttpClient) {}
    
     buscar(frase: string): Observable<string> {
   return this.http.get(this.apiUrl, {
@@ -21,6 +21,7 @@ private apiUrl = 'http://localhost:8080/html-link';
     responseType: 'text'
   });
 }
+
 
     
     // return this.http.get (`${this.apiUrl}/${frase}`);
