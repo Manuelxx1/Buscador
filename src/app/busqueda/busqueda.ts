3e22395f5ffad7deb5d86c1bab224e1fc282c619
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-29 04:06:53 -0300

    Update busqueda.ts

--- src/app/busqueda/busqueda.ts
+++ src/app/busqueda/busqueda.ts
@@ -20,7 +20,7 @@ busquedaControl = new FormControl('');
   isLoading = false;      // Para manejar el estado de carga
   error: string | null = null; // Para manejar errores
 campoinformaciondatabase:any;
-  palabradni:number;
+  palabradni!:number;
   // Inyecta el servicio en el constructor
   constructor(private Busquedaservice: Busquedaservice) { }
 
