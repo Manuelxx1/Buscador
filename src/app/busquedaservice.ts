a15359f29294b65174a4cd64088c0bfb2dfa327e
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-30 06:16:51 -0300

    Update busquedaservice.ts

--- src/app/busquedaservice.ts
+++ src/app/busquedaservice.ts
@@ -46,12 +46,10 @@ constructor(private http: HttpClient) { }
 obtenerPorId(dni: any): Observable<any> {
   console.log('DNI recibido en el servicio:', dni); //Esto lo ves en la consola del navegador
 
-  return this.http.get<any>(`${this.backendURL}/${dni}`).pipe(
-    map(respuesta => ({
-      ...respuesta,
-      dniRecibido: dni + "dni desde servicio"//Agregamos el DNI a la respuesta
-    }))
-  );
+  return this.http.get<any>(`${this.backendURL}/${dni}`);
+    
+    
+  
 }
 
 }
