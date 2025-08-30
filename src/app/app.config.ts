90696ac70cf212c8947cf19d174babf2736a4039
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-30 01:36:33 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { Busquedaservice } from '../busquedaservice';
+import { Busquedaservice } from './busquedaservice';
 
 import { provideHttpClient } from '@angular/common/http'; 
 import { routes } from './app.routes';
