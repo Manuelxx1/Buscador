8807e8ae7d0636893356a721b82e7d9dd210dc6d
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-09 04:46:09 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { Busqueda} from './busqueda/busqueda';
-import { App } from './app';
 
 import { NotFound} from './not-found/not-found';
 
 export const routes: Routes = [
-    { path: '', component: App },
+    
   { path: 'buscar', component: Busqueda },
   { path: '**', component: NotFound }
 
