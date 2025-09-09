19a6a3f8cba0f7c5d853a62c2e401e9af1edda43
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-09 06:28:42 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,12 @@ import { NotFound} from './not-found/not-found';
 import { App} from './app';
 
 export const routes: Routes = [
-      { path: '', redirectTo: '' ,pathMatch: 'full'   },
+
+// el path búsqueda es momentánea hasta que se configure 
+//  un rootcomponent para ello y también evitar crear 
+  // un component como home no planeado para rellenar ese path
+  // sólo para que funcione el 404
+      { path: '', component:Busqueda },
   { path: 'buscar', component: Busqueda },
   { path: '**', component: NotFound }
 
