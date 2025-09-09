caf448249df3cc2b24c3d683f269f9ddc6fb082a
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-09 05:19:40 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Busqueda} from './busqueda/busqueda';
 
 import { NotFound} from './not-found/not-found';
+import { App} from './app';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '/buscar', component: Busqueda },
+    { path: '', redirectTo: 'App', pathMatch: 'full' },
+  { path: 'buscar', component: Busqueda },
   { path: '**', component: NotFound }
 
 ];
