8c71a56a263d8125e3610a61d2e40ff227966863
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-09 05:24:51 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { NotFound} from './not-found/not-found';
 import { App} from './app';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'App', pathMatch: 'full' },
+    
   { path: 'buscar', component: Busqueda },
   { path: '**', component: NotFound }
 
