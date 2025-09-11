708601c42db8a1a2c9092dc34c4a4591aee75b05
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-11 06:50:33 -0300

    Update theme-toggle-component.ts

--- src/app/theme-toggle-component/theme-toggle-component.ts
+++ src/app/theme-toggle-component/theme-toggle-component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-theme-toggle-component',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './theme-toggle-component.html',
   styleUrl: './theme-toggle-component.css'
 })
 export class ThemeToggleComponent {
 
+icon = '🌙';
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark');
+      this.icon = '☀️';
+    }
+  }
+
+  toggleTheme() {
+    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.contains('dark');
+    this.icon = isDark ? '☀️' : '🌙';
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  }
+  
 }
