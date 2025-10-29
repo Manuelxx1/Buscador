2f718e7a2e683f3b5bd8ef92b7e33dc878c640da
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-29 05:22:45 -0300

    Add OnInit lifecycle and HTTP call in AuthCallback
    Implement authentication callback handling in AuthCallback component.

--- src/app/auth-callback/auth-callback.ts
+++ src/app/auth-callback/auth-callback.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth-callback',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './auth-callback.html',
   styleUrl: './auth-callback.css'
 })
-export class AuthCallback {
+export class AuthCallback implements OnInit{
+
+constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const code = this.route.snapshot.queryParamMap.get('code');
+    if (code) {
+      this.http.get(`http://localhost:3000/auth/callback?code=${code}`)
+        .subscribe({
+          next: (data) => {
+            console.log('Perfil:', data);
+            // Podés guardar los datos en localStorage o mostrar en pantalla
+          },
+          error: (err) => {
+            console.error('Error al autenticar:', err);
+          }
+        });
+    }
+  }
+
 
 }
