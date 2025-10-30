680f4a0cb30d24fc14f375de1114ee6ec19df9a7
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-30 04:27:27 -0300

    Refactor authentication callback logic

--- src/app/auth-callback/auth-callback.ts
+++ src/app/auth-callback/auth-callback.ts
@@ -11,31 +11,22 @@ import { CommonModule } from '@angular/common';
   styleUrl: './auth-callback.css'
 })
 export class AuthCallback implements OnInit{
-usuario: any = null;
+  usuario: any = null;
   error: string | null = null;
-constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
     const code = this.route.snapshot.queryParamMap.get('code');
-    if (code) {
-      this.http.get(`https://3000-cs-582739288523-default.cs-us-east1-yeah.cloudshell.dev/auth/callback?code=${code}`)
-        .subscribe({
-          next: (data) => {
-            console.log('Perfil:', data);
-            // Podés guardar los datos en localStorage o mostrar en pantalla
-         this.usuario = data;
+    const codeVerifier = 'challenge123'; // mismo valor que usaste en el login
 
-
-          },
-          error: (err) => {
-            console.error('Error al autenticar:', err);
-          this.error = 'Error al autenticar: ' + err.message;
-    
-          
-          }
-        });
-    }
+    this.http.post('https://3000-cs-582739288523-default.cs-us-east1-yeah.cloudshell.dev/auth/callback', {
+      code,
+      code_verifier: codeVerifier
+    }).subscribe({
+      next: (data) => this.usuario = data,
+      error: (err) => this.error = 'Error al autenticar: ' + err.message
+    });
   }
-
 
 }
