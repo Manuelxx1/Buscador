e5bda2f58192965ba1340f09679a8b118e018412
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-08 06:11:43 -0300

    Update busquedaservice.ts

--- src/app/busquedaservice.ts
+++ src/app/busquedaservice.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 //model para el carrito de compras 
-  import { Product } from '../models/product';
+  import { Product } from './models/product';
 
 import { of } from 'rxjs';
 
