9daf91cd2b7f7707354517f54bf84757ad1389e6
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-24 04:48:50 -0300

    Add FacebookComments component with SDK integration
    Implement FacebookComments component with SDK loading

--- src/app/facebook-comments/facebook-comments.ts
+++ src/app/facebook-comments/facebook-comments.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-facebook-comments',
@@ -6,6 +9,44 @@ import { Component } from '@angular/core';
   templateUrl: './facebook-comments.html',
   styleUrl: './facebook-comments.css'
 })
-export class FacebookComments {
+export class FacebookComments implements AfterViewInit {
+  
 
-}
+  // Recibimos la ruta del artículo desde el componente padre (ej: '/articulo-noticias/.../ciberseguridad2')
+  @Input() currentRoute: string = ''; 
+
+  // Tu dominio web definitivo (cuando lo subas). Para pruebas locales, Facebook suele usar localhost
+  private baseUrl = 'https://4200-cs-582739288523-default.cs-us-east1-pkhd.cloudshell.dev'; 
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  get fullUrl(): string {
+    return `${this.baseUrl}${this.currentRoute}`;
+  }
+
+  ngAfterViewInit(): void {
+    // Nos aseguramos de ejecutar esto solo en el navegador (evita errores si usás SSR)
+    if (isPlatformBrowser(this.platformId)) {
+      this.cargarOParsearSDK();
+    }
+  }
+
+  private cargarOParsearSDK() {
+    const win = window as any;
+
+    // Si el SDK de Facebook ya está cargado en el index.html, solo le pedimos que parsee el nuevo componente
+    if (win.FB) {
+      setTimeout(() => {
+        win.FB.XFBML.parse(); 
+      }, 100);
+    } else {
+      // Si no existe, creamos el script dinámicamente (reemplaza 'TU_APP_ID' si tenés una, sino funciona igual)
+      const script = document.createElement('script');
+      script.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v17.0';
+      script.async = true;
+      script.defer = true;
+      script.crossOrigin = 'anonymous';
+      document.body.appendChild(script);
+    }
+  }
+  }
