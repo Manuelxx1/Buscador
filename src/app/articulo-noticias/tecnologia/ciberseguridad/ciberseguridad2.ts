25468e1fa41da49a917360286b9d1ee0d9ccc643
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-26 13:49:24 -0300

    Import Comentarios in CiberSeguridad2 component

--- src/app/articulo-noticias/tecnologia/ciberseguridad/ciberseguridad2.ts
+++ src/app/articulo-noticias/tecnologia/ciberseguridad/ciberseguridad2.ts
@@ -1,10 +1,10 @@
 
 
 import { Component } from '@angular/core';
-
+import { Comentarios } from '../../../comentarios/comentarios';
 @Component({
   selector: 'app-ciberseguridad2',
-  imports: [],
+  imports: [Comentarios],
   templateUrl: './ciberseguridad2.html',
   styleUrl: './ciberseguridad2.css',
 })
